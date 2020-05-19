a0483dd089d058e18c9578d19b3eb2501137962a
João Vitor Oliveira Araujo <62951929+ojoaovitoraraujo@users.noreply.github.com>
2020-05-19 18:24:00 -0300

    add % function

--- Lista6/c/2.c
+++ Lista6/c/2.c
@@ -2,47 +2,44 @@
 #include <stdlib.h>
 
 int main(){
-  int i,j, alacertos[10],aux=0, num;
-  char gabarito[8], alresp[8];
 
-//GRAVAR AS RESPOSTAS
-  for(i=0; i<8; i++){
-    printf("Digite a resposta [%d] do gabarito: ", i+1);
-    scanf(" %c", &gabarito[i]);
-  }
+    int i,num,j,note=0,cont=0;
+    float porcentagem,aux;
+    char gabarito[8];
 
-  printf("\n\n========== ALUNOS ==========\n\n");
-
-  printf("numero de alunos: ");
-  scanf("%d", &num);
-//CONTA OS ALUNOS
-  for(i=0;i<num;i++){
-
-//PEGA RESPOSTAS DOS ALUNOS
-    for(j=0;j<8;j++){
-      printf("resposta do aluno %d na questao %d: ", i+1,j+1);
-      setbuf(stdin, NULL);
-      scanf(" %c", &alresp[j]);
-      
-//VERIFICA SE O ALUNO ACERTOU
-      if(alresp[j] == gabarito[j]){
-        alacertos[i]++;
-      }
+    for(i=0 ; i<8 ; i++){
+        printf("Informe a resposta da %d: ",i+1);
+        setbuf(stdin, NULL);
+        scanf("%c",&gabarito[i]);
     }
-    if(alacertos[i] > 6){
-      aux++;
+
+    printf("\n\nInforme a quantidade de alunos: ");
+    scanf("%d",&num);
+
+    char respostas[num];
+  
+    for(i=0 ; i<num ; i++){
+            printf("\n========ALUNO %d========\n",i+1);
+        for(j=0 ; j<8 ; j++){
+            printf("Resposta da Questão %d = ",j+1);
+            setbuf(stdin, NULL);
+            scanf("%c",&respostas[j]);
+        
+            if(gabarito[j] == respostas[j]){
+                note++;
+            }
+        }
+        if(note>=6){
+            printf("\nAluno %d APROVADO - Nota: %d\n\n",i+1,note);
+            cont++;
+        } else {
+            printf("\nAluno %d REPROVADO - Nota: %d\n\n",i+1,note);
+        }
+        note=0;
     }
-    printf("\n\n");
-  } 
-  alacertos[0]--;
+    aux = cont;
+    porcentagem = aux/num;
 
-//MOSTA RESPOSTAS
-  printf("\n\n========== RESPOSTAS ==========\n\n");
-  for(i=0;i<num;i++){
-    printf("Aluno %d, respostas certas: %d \n", i+1, alacertos[i]);
-  }
-  printf("APROVADOS: %d%% ", (aux/num)*100);
+    printf("APROVACAO = %.2f%%",porcentagem*100);
 
-  return 0;
 }
-
